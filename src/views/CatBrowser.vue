644d88189cc3528e6ac907fe3dd509c46de48ff7
Robert Anthony Tribiana <ratribiana@gmail.com>
2021-01-23 04:37:30 +0800

    Feat: Add methods and dispatch to get cats breed

--- src/views/CatBrowser.vue
+++ src/views/CatBrowser.vue
@@ -8,10 +8,17 @@
           <div class="row pt-4">
             <div class="col-lg-3 col-md-3 col-sm-12">
               <label class="w-100 form-control-label" for="role">Select Breed </label>
-
+              <b-form-select
+                name="breed"
+                v-model="breed"
+                :options="breeds"
+                :disabled="catBreedLoading"
+                @change="getCats(page, limit, $event)"
+                class="form-control custom-select"
+              >
+              </b-form-select>
             </div>
           </div>
-
         </div>
       </div>
     </div>
@@ -27,11 +34,51 @@
     data () {
       return {
         title: '',
+        breed: null,
+        catList: [],
+        page: 1,
+        limit: 10
+      }
+    },
+    computed: {
+      catBreedLoading () {
+        // Loading state for cat breeds
+        return this.$store.state.cats.catBreedLoading
+      },
+      breeds () {
+        // Computed property for cat breeds (reactive)
+        let data = []
+
+        // Reconstruct to array of object to be used in select options
+        this.breedList.map(breed => {
+          data.push({ value: breed.id, text: breed.name })
+        })
+
+        return [
+          { value: null, text: 'Please select cat breed', disabled: true },
+          ...data
+        ]
+      },
+      breedList: {
+        // list of breeds that came from store
+        get: function () {
+          return this.$store.state.cats.catBreeds || { value: null, text: 'Please select cat breed', disabled: true }
+        },
+        set: function (newValue) {
+          return newValue
+        }
+      }
+    },
+    methods: {
+      getCatBreeds () {
+        // Dispatch cat breeds action to fetch the data from API endpoint
+        this.$store.dispatch('cats/GET_CAT_BREEDS')
       }
     },
     mounted () {
       // Assigned route name as the title of the page
       this.title = this.$route.name
+      this.getCatBreeds()
     }
   }
 </script>
--- src/views/CatDetails.vue
+++ src/views/CatDetails.vue
@@ -39,58 +39,10 @@
       }
     },
     methods: {
-      getCat (catID) {
-        // Get list of cats from store
-        const catList = this.$store.state.cats.catList
 
-        // Check if catList has value or there are objects assigned to catList
-        if (catList.length > 0) {
-          const cat = catList.filter( cat => {
-            return cat.id === catID
-          })
-
-          // if there are objects assigned, put it to the cat variable
-          if (cat && cat.length > 0) {
-            this.cat = {
-              url: cat[0].url,
-              breedID: 'breeds' in cat[0] && cat[0].breeds.length > 0 ? cat[0].breeds[0].id : '',
-              name: 'breeds' in cat[0] && cat[0].breeds.length > 0 ? cat[0].breeds[0].name : '',
-              origin: 'breeds' in cat[0] && cat[0].breeds.length > 0 ? cat[0].breeds[0].origin : '',
-              temperament: 'breeds' in cat[0] && cat[0].breeds.length > 0 ? cat[0].breeds[0].temperament : '',
-              description: 'breeds' in cat[0] && cat[0].breeds.length > 0 ? cat[0].breeds[0].description : ''
-            }
-          }
-        } else {
-          // iF there are NO objects assigned to catList in store, dispatch an action for getting the cat info using
-          this.$store.dispatch('cats/GET_CAT_BY_ID', catID)
-            .then((response) => {
-              const catFromAPI = this.$store.state.cats.cat
-
-              // Check if cat from store has value
-              if (catFromAPI && Object.keys(catFromAPI).length > 0) {
-                // if yes, assign the value of cat from the store
-                this.cat = {
-                  url: catFromAPI.url,
-                  breedID: 'breeds' in catFromAPI && catFromAPI.breeds.length > 0 ? catFromAPI.breeds[0].id : '',
-                  name: 'breeds' in catFromAPI && catFromAPI.breeds.length > 0 ? catFromAPI.breeds[0].name : '',
-                  origin: 'breeds' in catFromAPI && catFromAPI.breeds.length > 0 ? catFromAPI.breeds[0].origin : '',
-                  temperament: 'breeds' in catFromAPI && catFromAPI.breeds.length > 0 ? catFromAPI.breeds[0].temperament : '',
-                  description: 'breeds' in catFromAPI && catFromAPI.breeds.length > 0 ? catFromAPI.breeds[0].description : ''
-                }
-              }
-            }).catch((err) => {
-              // Handle error
-              console.error(err)
-              err.then(error => {
-                console.error('ERROR:', { error })
-              })
-            })
-        }
-      }
     },
     mounted () {
-      // Call the methods that will dispatch an action for getting cat Data when the page is mounted
-      this.getCat(this.$route.params.catID)
+
     }
   }
 </script>
